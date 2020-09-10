3bfbacc7c1b00d3a7ab4f8df7642242b0fa65c33
Nishan <nishanbhowmick@gmail.com>
2020-09-10 17:19:50 +0530

    Registration page added

--- TestCommonGit/src/com/lti/Login.java
+++ TestCommonGit/src/com/lti/Login.java
@@ -4,4 +4,8 @@ public class Login {
 	public void login() {
 		System.out.println("Login Page is Added");
 	}
+	public void registration()
+	{
+		System.out.println("Registration Page is Added");
+	}
 }
